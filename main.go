6e68e005790d9c307dcf934ca668b17000d5d9f4
siddharth_dumbre <dumbre.si@northeastern.edu>
2024-09-22 15:57:28 -0400

    Added logic for connection to database

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"log"
-
+	"os"
+	"github.com/CSYE-6225-CLOUD-SIDDHARTH/webapp/storage"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"gorm.io/gorm"
@@ -12,6 +13,12 @@ type Repository struct{
 	DB *gorm.DB
 }
 
+func(r *Repository) SetUpRoutes(app *fiber.App){
+	app.Get("/healthz", func(c *fiber.Ctx) error{
+		c.Status(fiber.StatusOK)
+		return nil
+	})
+}
 func main(){
 
 	err:=godotenv.Load(".env")
@@ -20,12 +27,23 @@ func main(){
 		log.Fatal(err)
 	}
 
+	config:=&storage.Config{
+		Host: os.Getenv("DB_Host"),
+		Port: os.Getenv("DB_Port"),
+		User: os.Getenv("DB_User"),
+		Password: os.Getenv("DB_Password"),
+		DbName: os.Getenv("DB_Name"),
+		SSLMode: os.Getenv("DB_SslMode"),
+	}
+	db,err:= storage.NewConnection(config)
+
+	if(err!=nil){
+		log.Fatal(err.Error())
+	}
+	r:=Repository{
+		DB: db,
+	}
 	app:=fiber.New()
-	app.Get("/healthz", func(c *fiber.Ctx) error{
-		if(c!=nil){
-			return c.SendStatus(fiber.StatusOK)
-		}
-		return nil
-	  })
+	r.SetUpRoutes(app)
 	app.Listen(":3000")
 }
