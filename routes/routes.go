73e8096c62a1768170b85a1bb6bdbb2dde3cc3a6
siddharth_dumbre <dumbre.si@northeastern.edu>
2024-09-30 22:04:13 -0400

    Update user api implementation

--- routes/routes.go
+++ routes/routes.go
@@ -11,4 +11,5 @@ func SetupRoutes(app *fiber.App) {
 	app.All("healthz/*", controllers.ErrorHealthCheck)
 	app.Get("v1/user/self",middleware.BasicAuthMiddleware,controllers.GetUser)
 	app.Post("/v1/user",controllers.CreateUser)
+	app.Put("v1/user/self",middleware.BasicAuthMiddleware,controllers.UpdateUser)
 }
