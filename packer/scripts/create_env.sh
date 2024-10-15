6b8428bb45a4fc3539930d1c045fcbda2364fcac
siddharth_dumbre <dumbre.si@northeastern.edu>
2024-10-15 15:05:21 -0400

    Port typo fixed in all files

--- packer/scripts/create_env.sh
+++ packer/scripts/create_env.sh
@@ -5,4 +5,4 @@
 # echo "DB_Password=${{ secrets.DB_Password }}" >> .env
 # echo "DB_Name=${{ secrets.DB_Name }}" >> .env
 # echo "DB_Host="localhost"" >> .env
-# echo "DB_PORT=5432" >> .env
+# echo "DB_Port=5432" >> .env
