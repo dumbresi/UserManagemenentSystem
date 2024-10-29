37ba9cdfedab965a048ce5bca7f0df693d89b354
siddharth_dumbre <dumbre.si@northeastern.edu>
2024-10-29 14:26:06 -0400

    cloudwatch agent not found-fix

--- packer/scripts/installCloudWatch.sh
+++ packer/scripts/installCloudWatch.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install -y amazon-cloudwatch-agent
+# sudo apt-get install -y amazon-cloudwatch-agent
+sudo apt-get update
+sudo apt-get install -y wget
+wget -qO - https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i amazon-cloudwatch-agent.deb
+
