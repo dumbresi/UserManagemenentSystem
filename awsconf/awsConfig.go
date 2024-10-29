71c9fc48e81ce27e467e1bb71bed981050bfffdf
siddharth_dumbre <dumbre.si@northeastern.edu>
2024-10-28 22:02:12 -0400

    aws sdk bug fix

--- awsconf/awsConfig.go
+++ awsconf/awsConfig.go
@@ -6,17 +6,11 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/joho/godotenv"
 )
 
 func InitS3Client() *s3.Client {
-
-    err := godotenv.Load("../.env")
-    if err != nil {
-        log.Fatalf("Error loading .env file")
-    }
     
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
+	cfg, err := config.LoadDefaultConfig(context.TODO(),config.WithSharedConfigProfile("dev"),config.WithRegion("us-east-1"))
 	if err != nil {
 		log.Printf("unable to load SDK config, %v", err)
 	}
