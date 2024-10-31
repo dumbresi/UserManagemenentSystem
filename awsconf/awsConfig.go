077b12f8582d3740c54893ec7aa50095a5907fd9
siddharth_dumbre <dumbre.si@northeastern.edu>
2024-10-31 02:07:17 -0400

    env right path

--- awsconf/awsConfig.go
+++ awsconf/awsConfig.go
@@ -14,9 +14,10 @@ var bucketName string
 
 func InitS3Client() *s3.Client {
 
-    err:=godotenv.Load(".env")
+    err:=godotenv.Load("../.env")
 	if(err!=nil){
 		log.Error().Err(err).Msg("Error loading Env")
+		return nil
 	}
 
 	bucketName= os.Getenv("S3_Bucket_Name")
